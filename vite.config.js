8590f034b54b953785b07abb056b8f55f183e2f8
CarlosMedinaSullca <carlos.medina.sullca@gmail.com>
2025-02-18 17:30:50 -0400

    vite updating

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        food: resolve(__dirname, "src/food/index.html"),
+        listing: resolve(__dirname, "src/search_listing/index.html")
       },
     },
   },
